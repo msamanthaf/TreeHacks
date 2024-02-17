ead2fba51a647fd6bfc09270609c03976b92336e
Flayre <mariesamantha.f@gmail.com>
2024-02-17 08:35:33 -0800

    Modified frontend

--- web3/Components/Payment.jsx
+++ web3/Components/Payment.jsx
@@ -54,7 +54,7 @@ const Payment = ({ setOpenModel, pay, payFunction }) => {
 
               <input
                 onChange={(e) => setAmount(e.target.value)}
-                placeholder="amount"
+                placeholder="Amount"
                 required
                 type="text"
                 className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
